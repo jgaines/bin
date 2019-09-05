45d3b2cc21f0ce67209b284c9256fb9acf37b22d
John Gaines <jgaines@arbor.net>
2019-09-05 12:51:33 -0400

    removed goto (has it's on repo now), updated flip.py

--- flip.py
+++ flip.py
@@ -28,33 +28,92 @@ function goto {
 }
 
 """
+import configparser
+import csv
+from collections import defaultdict
+from io import StringIO
 import os
 import pathlib
 import sys
+from typing import Any, Dict, List, Tuple
 
+from xdg import BaseDirectory
+
+
+# TODO: either side of the FLIPS list should allow multiple targets, so:
+#           administrator -> eng-tools, docker
+#       should be a valid flip entry, meaning that from administrator,
+#       first look in eng-tools then docker for a match and going the other way
+#       both eng-tools and docker flip back to administrator.
+#       FLIP_FLOP should be Dict[str, List[str]]
+#       The code for building FLIP_FLOP from FLIPS will probably have to be done
+#       with nested for loops to make it less confusing to follow.
 
 FLIPS = {  # TODO: This should probably be in an external config
     ('administrator', 'eng-tools'),
 }
+
+
 FLIP_FLOP = {k: v for k, v in FLIPS}
 FLIP_FLOP.update((v, k) for k, v in FLIPS)
 
 
+def load_config(name:str=None) -> Tuple[Dict[str, Any], Dict[str, List[str]]]:
+    """Attempt to load config."""
+    options = dict()
+    flips = dict()
+    config = None
+    for name in BaseDirectory.load_config_paths('flip'):
+        config_file = pathlib.Path(name) / 'flip.cfg'
+        if config_file.exists():
+            config = configparser.ConfigParser()
+            config.read(config_file)
+            break
+    if config:
+        if 'options' in config:
+            options = dict(config['options'])
+        if 'flips' in config:
+            for src in config['flips']:
+                dst = [d.strip() for d in next(csv.reader(StringIO(config['flips'][src])))]
+                flips[src] = dst
+    return options, flips
+
+
+def make_flip_flops(flips: Dict[str, List[str]]) -> Dict[str, List[str]]:
+    flip_flops = defaultdict(list)
+    flip_flops.update(flips)
+    for src, destinations in flips.items():
+        for dst in destinations:
+            if src not in flip_flops[dst]:
+                flip_flops[dst].append(src)
+    return flip_flops
+
+
 def flip(_args):
+    options, flips = load_config()
+
+    flip_flops = make_flip_flops(flips)
+
     current_dir = pathlib.Path(os.getcwd())
+    # TODO: Add code to try stepping up the dir tree until we find a point we
+    # can flip or hit home folder or root or some such.
+    # home_dir = path
+    # other_parent = FLIP_FLOP.get(current_dir.parent.name, None)
+    # while other_parent is None and current_dir
     try:
-        other_parent = FLIP_FLOP[current_dir.parent.name]
+        other_parents = flip_flops[current_dir.parent.name]
     except KeyError:
         print("Couldn't figure out how to flip from here.")
         return 1
     else:
-        other_dir = current_dir.parent.parent / other_parent / current_dir.name
-        if other_dir.exists():
-            with open(os.path.expanduser('~/.flip_directory'), 'w') as f:
+        for other_parent in other_parents:
+            other_dir = current_dir.parent.parent / other_parent / current_dir.name
+            if other_dir.exists():
+                with open(os.path.expanduser('~/.flip_directory'), 'w') as f:
                     f.write(str(other_dir))
-            return 0
+                return 0
         else:
-            print(f"{other_dir} doesn't exist")
+            print(f"couldn't find {other_dir} in {', '.join(other_parents)}.")
             return 1
 
 
