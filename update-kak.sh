69df49fed3f58da2591b18e5377db45a311a2b77
John Gaines <john.gaines@netscout.com>
2025-08-07 11:22:26 -0400

    mucking about with check and update scripts

---
+++ update-kak.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+cd $HOME/src/kakoune
+git pull
+make
+make PREFIX=$HOME/.local install
